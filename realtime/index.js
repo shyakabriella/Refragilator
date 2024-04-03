2d41b0ee0ddb4b258f637a616d539aa3eefff642
Your <paci3ishimwe@gmail.com>
2024-04-03 14:50:36 +0200

    hhs

---
+++ realtime/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var http = require("http");
+var io = require('socket.io')(server);
+var mysql = require('mysql');
+
+app.use(express.static(__dirname + '/client'));
+
+app.get('/', function (req, res, next) {
+    console.log(req.params)
+    res.sendFile(__dirname + '/admin.html');
+});
+
+io.on('connection', function (socket) {
+    console.log("hhh");
+    socket.on("add_me", function(msg){
+        console.log(msg);
+    })
+});
+
+server.listen(8080, () => {
+    console.log('real time refrigerat is on 8080');
+});
